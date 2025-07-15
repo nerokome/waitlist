22041816ab1ae66fee3887e3e9b254261b8f485d
nerokome <nerooghenekome@gmail.com>
2025-07-15 13:24:02 +0100

    me

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       const data = await res.json();
 
       if (res.ok) {
-        setMessage("✅ You’ve been added to the waitlist!");
+        setMessage("✅ Thank you for subscribing to the waitlist!");
         setEmail("");
       } else {
         setMessage(data?.error || "Something went wrong.");
@@ -51,9 +51,9 @@ export default function Home() {
           alt="Logo"
           width={40}
           height={40}
-          className="w-9 h-9 rounded-full object-cover"
+          className="w-10 h-10 rounded-full object-cover"
         />
-        <p className="text-3xl sm:text-2xl md:text-4xl font-bold text-blue-600">
+        <p className="text-xl sm:text-2xl md:text-4xl font-bold text-blue-600">
           ScholarGuide Tech
         </p>
       </div>
